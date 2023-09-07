5b6b080128d3809b85a39cb5b310b7bce995ea22
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-09-07 21:27:44 +0200

    SpringConfigCloud module added
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

---
+++ spring-config-cloud/src/main/java/pl/flywithbookedseats/springconfigcloud/SpringConfigCloud.java
@@ -0,0 +1,12 @@
+package pl.flywithbookedseats.springconfigcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigCloud {
+
+    public static void main(String[] ars) {
+        SpringApplication.run(SpringConfigCloud.class, ars);
+    }
+}
---
+++ spring-config-cloud/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $pl.flywithbookedseats;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
