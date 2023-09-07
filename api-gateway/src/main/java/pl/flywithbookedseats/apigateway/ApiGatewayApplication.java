499fbf998d723cf585126291652b1c4707c7b28c
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-09-07 17:41:14 +0200

    Dependency managemenr in parent pom added
    Resolving dependency problem
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- api-gateway/src/main/java/pl/flywithbookedseats/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/pl/flywithbookedseats/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,11 @@
 package pl.flywithbookedseats.apigateway;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+//@SpringBootApplication
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        //SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
--- api-gateway/src/main/java/pl/flywithbookedseats/apigateway/configuration/LoggingFilter.java
+++ api-gateway/src/main/java/pl/flywithbookedseats/apigateway/configuration/LoggingFilter.java
@@ -1,12 +1,11 @@
 package pl.flywithbookedseats.apigateway.configuration;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+//@RestController
 public class LoggingFilter {
 
-    @GetMapping(path = "/test")
+   // @GetMapping(path = "/test")
     public String test() {
         return "test";
     }
