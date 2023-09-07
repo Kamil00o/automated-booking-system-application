3d987c3d3b597625798074e9ed5a434253e9f2bf
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-09-07 23:25:14 +0200

    Passenger account service module has been added
    Whithin this commit:
    -Passenger-account-service basic structure with properties
    has been added
    -Eureka server properties has been adjusted to register added
    Passenger-account-service
    -Spring security, h2, openfeign dependencies have been added &
    their version has been adjusted in dependency management section
    in parent pom.xml
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

---
+++ passenger-account-service/src/main/java/pl/flywithbookedseats/passengeraccountservice/PassegerAccountServiceApplication.java
@@ -0,0 +1,15 @@
+package pl.flywithbookedseats.passengeraccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class PassegerAccountServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PassegerAccountServiceApplication.class, args);
+    }
+}
