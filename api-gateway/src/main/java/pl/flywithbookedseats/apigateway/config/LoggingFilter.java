b6cd52f9c90cbf24722bc6302a6541fe9e52c361
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-09-08 02:43:30 +0200

    PassengerAccount Entity added, PSQL DB connection
    Entity for the PassengerAccount domain has been created.
    Required dependencies have been added & connection with
    passenger_account_db in the container has been established
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- api-gateway/src/main/java/pl/flywithbookedseats/apigateway/config/LoggingFilter.java
+++ api-gateway/src/main/java/pl/flywithbookedseats/apigateway/config/LoggingFilter.java
@@ -6,8 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
--- passenger-account-service/src/main/java/pl/flywithbookedseats/passengeraccountservice/model/domain/PassengerAccount.java
+++ passenger-account-service/src/main/java/pl/flywithbookedseats/passengeraccountservice/model/domain/PassengerAccount.java
@@ -1,4 +1,117 @@
 package pl.flywithbookedseats.passengeraccountservice.model.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
 public class PassengerAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private LocalDate birthDate;
+    private String healthState;
+    private String lifeStage;
+    private List<PassengerAccount> accompangingPassengers;
+
+    protected PassengerAccount() {
+
+    }
+
+    public PassengerAccount(Long id, String name, String surname, String email, LocalDate birthDate, String healthState, String lifeStage, List<PassengerAccount> accompangingPassengers) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.healthState = healthState;
+        this.lifeStage = lifeStage;
+        this.accompangingPassengers = accompangingPassengers;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getHealthState() {
+        return healthState;
+    }
+
+    public void setHealthState(String healthState) {
+        this.healthState = healthState;
+    }
+
+    public String getLifeStage() {
+        return lifeStage;
+    }
+
+    public void setLifeStage(String lifeStage) {
+        this.lifeStage = lifeStage;
+    }
+
+    public List<PassengerAccount> getAccompangingPassengers() {
+        return accompangingPassengers;
+    }
+
+    public void setAccompangingPassengers(List<PassengerAccount> accompangingPassengers) {
+        this.accompangingPassengers = accompangingPassengers;
+    }
+
+    @Override
+    public String toString() {
+        return "PassengerAccount{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", healthState='" + healthState + '\'' +
+                ", lifeStage='" + lifeStage + '\'' +
+                ", accompangingPassengers=" + accompangingPassengers +
+                '}';
+    }
 }
