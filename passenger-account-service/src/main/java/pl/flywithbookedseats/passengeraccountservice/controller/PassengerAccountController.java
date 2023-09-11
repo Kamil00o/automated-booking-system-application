b5bed33f9f3e705e51268c4d46520b001c93949a
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-09-11 02:19:34 +0200

    Service's logic has been moved to @Service class
    passenger-account-service business logic has been moved from Controller
    class to @Service class.
    
    [CreatePassengerAccount]: all methods has been moved from controller
    class. @RequiredArgsConstructor has been added instead of @Autowired
    
    [PassengerAccountController]: business logic has been moved to @Service
    class. @RequiredArgsConstructor has been added instead of @Autowired
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- passenger-account-service/src/main/java/pl/flywithbookedseats/passengeraccountservice/controller/PassengerAccountController.java
+++ passenger-account-service/src/main/java/pl/flywithbookedseats/passengeraccountservice/controller/PassengerAccountController.java
@@ -1,35 +1,23 @@
 package pl.flywithbookedseats.passengeraccountservice.controller;
 
-import jakarta.persistence.Column;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import pl.flywithbookedseats.passengeraccountservice.exceptions.PassengerAccountAlreadyExistsException;
 import pl.flywithbookedseats.passengeraccountservice.model.command.UpdatePassengerAccount;
 import pl.flywithbookedseats.passengeraccountservice.model.domain.PassengerAccount;
-import pl.flywithbookedseats.passengeraccountservice.exceptions.PassengerAccountNotFoundException;
-import pl.flywithbookedseats.passengeraccountservice.repository.PassengerAccountRepository;
+import pl.flywithbookedseats.passengeraccountservice.service.PassengerAccountService;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/passenger-account")
 public class PassengerAccountController {
 
-    private static final Logger logger = LoggerFactory.getLogger(PassengerAccountController.class);
-    private static final String PASSENGER_ACCOUNT_NOT_FOUND = "Passenger account with specified %s not found!";
-    private static final String PASSENGER_ACCOUNT_WITH_SPECIFIED_EMAIL_EXISTS =
-            "Passenger account with specified email: %s already exists!";
-
-    @Autowired
-    private PassengerAccountRepository passengerAccountRepository;
+    private final PassengerAccountService passengerAccountService;
 
     @GetMapping(path = "/test")
     public String getTestString() {
@@ -37,54 +25,24 @@ public class PassengerAccountController {
     }
 
     @GetMapping(path = "/all")
-    @Transactional
     public List<PassengerAccount> retrieveAllPassengerAccountsFromDb() {
-        return passengerAccountRepository.findAll();
+        return passengerAccountService.retrieveAllPassengerAccountsFromDb();
     }
 
     @GetMapping(path = "/id/{id}")
     public Optional<PassengerAccount> getPassengerAccountById(@PathVariable Long id) {
-        Optional<PassengerAccount> passengerAccount = passengerAccountRepository.findById(id);
-
-        if (passengerAccount.isEmpty()) {
-            throw new PassengerAccountNotFoundException(
-                    "Passenger account with specified id does not exist id: %s\", id");
-        }
-
-        return passengerAccount;
+        return passengerAccountService.getPassengerAccountById(id);
     }
 
     @PostMapping(path = "/create")
     public ResponseEntity<Object> createNewPassengerAccount(@Valid @RequestBody PassengerAccount passengerAccount) {
-        logger.info("New passenger account for {} {} is being created!", passengerAccount.getName()
-                ,passengerAccount.getSurname());
-        PassengerAccount savedPassengerAccount = passengerAccountRepository.save(passengerAccount);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(savedPassengerAccount.getId())
-                .toUri()
-                .normalize();
-        return ResponseEntity.created(location).build();
+        return passengerAccountService.createNewPassengerAccount(passengerAccount);
     }
 
     @PutMapping(path = "/edit/{id}")
     @Transactional
     public void editPassengerAccount(@PathVariable long id,
                                      @Valid @RequestBody UpdatePassengerAccount updatePassengerAccount) {
-        PassengerAccount passengerAccount = passengerAccountRepository.findById(id)
-                .orElseThrow(() -> new PassengerAccountNotFoundException(PASSENGER_ACCOUNT_NOT_FOUND.formatted(id)));
-
-        if (!passengerAccountRepository.existsByEmail(updatePassengerAccount.email())) {
-            passengerAccount.setName(updatePassengerAccount.name());
-            passengerAccount.setSurname(updatePassengerAccount.surname());
-            passengerAccount.setEmail(updatePassengerAccount.email());
-            passengerAccount.setBirthDate(updatePassengerAccount.birthDate());
-            passengerAccount.setHealthState(updatePassengerAccount.healthState());
-            passengerAccount.setLifeStage(updatePassengerAccount.lifeStage());
-        } else {
-            throw new PassengerAccountAlreadyExistsException(PASSENGER_ACCOUNT_WITH_SPECIFIED_EMAIL_EXISTS
-                    .formatted(updatePassengerAccount.email()));
-        }
+        passengerAccountService.editPassengerAccount(id, updatePassengerAccount);
     }
 }
