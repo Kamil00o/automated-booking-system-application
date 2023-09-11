b9400c86ab0bd9e72d1357a0dab9ec119efbb5b6
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-09-11 23:39:53 +0200

    Basic Mapped mathods in Controller added
    Basic mapped methods have been added as an initial pattern for future
    logic in @Service class
    
    [SeatsBookingSystemController]: Basic methods with basic operations
    added, mappings for new methods added
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/model/domain/SeatsScheme.java
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/model/domain/SeatsScheme.java
@@ -1,7 +1,14 @@
 package pl.flywithbookedseats.seatsbookingsystemservice.model.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
 
 @Entity
+@Builder
+@Table(
+        name = "seats_scheme_TABLE"
+)
 public class SeatsScheme {
+
 }
