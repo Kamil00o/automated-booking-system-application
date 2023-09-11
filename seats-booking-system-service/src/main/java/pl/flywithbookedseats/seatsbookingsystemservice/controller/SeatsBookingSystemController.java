432d1ebdeb45b9be25f48db03378a33f6433044f
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-09-11 18:09:27 +0200

    spring-cloud-starter-bootstrap-version
    spring-cloud-starter-bootstrap dependency added in parent
    pom.xml to fix compilation error during compilation of the services.
    
    Test method has been added in the seats-booking-system-service service
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/controller/SeatsBookingSystemController.java
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/controller/SeatsBookingSystemController.java
@@ -1,6 +1,7 @@
 package pl.flywithbookedseats.seatsbookingsystemservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.flywithbookedseats.seatsbookingsystemservice.service.SeatsBookingSystemService;
@@ -11,4 +12,9 @@ import pl.flywithbookedseats.seatsbookingsystemservice.service.SeatsBookingSyste
 public class SeatsBookingSystemController {
 
     private final SeatsBookingSystemService seatsBookingSystemService;
+
+    @GetMapping(path = "/test")
+    public String test() {
+        return "TEST";
+    }
 }
