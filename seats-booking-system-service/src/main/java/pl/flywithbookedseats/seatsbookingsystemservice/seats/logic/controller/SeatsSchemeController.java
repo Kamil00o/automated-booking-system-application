06f68f2f6e9157284ceeb6adca6aa412f27f6605
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-09-12 03:26:44 +0200

    Basic methods in SeatsSchemeController added
    Basic methods in SeatsSchemeController have been added & mapped. New
    CreateSeatsScheme record has been add to enbale creating create method
    for SeatsScheme entity by added controller's method
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/seats/logic/controller/SeatsSchemeController.java
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/seats/logic/controller/SeatsSchemeController.java
@@ -1,10 +1,14 @@
 package pl.flywithbookedseats.seatsbookingsystemservice.seats.logic.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.flywithbookedseats.seatsbookingsystemservice.seats.logic.model.command.CreateSeatsScheme;
+import pl.flywithbookedseats.seatsbookingsystemservice.seats.logic.model.domain.SeatsScheme;
 import pl.flywithbookedseats.seatsbookingsystemservice.seats.logic.service.SeatsSchemeService;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +20,20 @@ public class SeatsSchemeController {
     @GetMapping(path = "/test")
     public String test() {
         return "test";
+    }
+
+    @GetMapping(path = "/all")
+    public List<SeatsScheme> retrieveAllSeatsSchemes() {
+        return new LinkedList<>();
+    }
+
+    @PostMapping(path = "/create")
+    public SeatsScheme createNewSeatsScheme(@Valid @RequestBody CreateSeatsScheme createSeatsScheme) {
+        return new SeatsScheme();
+    }
+
+    @GetMapping(path = "/get/by-plane-model/{planeModel}")
+    public SeatsScheme retireveSeatsSchemeByPlaneModel(@PathVariable String planeModel) {
+        return new SeatsScheme();
     }
 }
