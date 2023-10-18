00ff621f5378bc1bbfa5b00779514f76a200ae50
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-10-18 22:07:11 +0200

    CreateSeatsSchemeModelCommand added
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

---
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/flight/logic/model/command/CreateSeatsSchemeModelCommand.java
@@ -0,0 +1,25 @@
+package pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.model.command;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.relational.core.sql.In;
+
+import java.util.List;
+
+import static pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.common.Consts.NOT_NULL_MESSAGE;
+
+@Builder
+@Getter
+@Setter
+public record CreateSeatsSchemeModelCommand(
+        @NotNull(message = NOT_NULL_MESSAGE)
+        String planeModel,
+        @NotNull(message = NOT_NULL_MESSAGE)
+        List<String> seatClassType,
+        @NotNull(message = NOT_NULL_MESSAGE)
+        List<Integer> amountOfSeatsPerSeatClassType,
+        @NotNull(message = NOT_NULL_MESSAGE)
+        List<Integer> amountOfRowsPerSeatClassType
+) {}
