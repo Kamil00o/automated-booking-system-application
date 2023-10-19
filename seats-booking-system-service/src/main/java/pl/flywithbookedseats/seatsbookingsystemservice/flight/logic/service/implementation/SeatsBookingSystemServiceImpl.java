0414b2c24b2179c49a08a844d653465d745f0aec
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-10-19 17:25:15 +0200

    Methods added in SeatsBookingSystemServiceImpl
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/flight/logic/service/implementation/SeatsBookingSystemServiceImpl.java
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/flight/logic/service/implementation/SeatsBookingSystemServiceImpl.java
@@ -2,8 +2,40 @@ package pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.service.imp
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.model.command.CreateSeatsSchemeModelCommand;
+import pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.model.dto.SeatsSchemeModelDto;
+import pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.repository.SeatsSchemeModelRepository;
+import pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.service.SeatsBookingSystemService;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class SeatsBookingSystemServiceImpl {
+public class SeatsBookingSystemServiceImpl implements SeatsBookingSystemService {
+
+    private final SeatsSchemeModelRepository seatsSchemeModelRepository;
+    @Override
+    public void addNewSeatsSchemeModel(CreateSeatsSchemeModelCommand createSeatsSchemeModelCommand) {
+
+    }
+
+    @Override
+    public List<SeatsSchemeModelDto> retrieveAllSavedSeatsSchemeModelsFromDb() {
+        return null;
+    }
+
+    @Override
+    public SeatsSchemeModelDto retrieveSeatsSchemeModelByPlaneModel(String planeModel) {
+        return null;
+    }
+
+    @Override
+    public SeatsSchemeModelDto retrieveSeatsSchemeModelById(Long id) {
+        return null;
+    }
+
+    @Override
+    public SeatsSchemeModelDto updateSeatsSchemeModel(Long id) {
+        return null;
+    }
 }
