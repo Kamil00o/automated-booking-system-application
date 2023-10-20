85014f4c0aa9c8c638febce029b3be917b522c1e
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-10-20 18:10:32 +0200

    addNewSeatsSchemeModel() Post method added
    addNewSeatsSchemeModel() Post method added has been added to
    SeatsBookingSystemController.
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/flight/logic/controller/SeatsBookingSystemController.java
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/flight/logic/controller/SeatsBookingSystemController.java
@@ -2,8 +2,11 @@ package pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.model.command.CreateFlight;
+import pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.model.command.CreateSeatsSchemeModelCommand;
 import pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.model.command.UpdateFlight;
 import pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.model.domain.Flight;
 import pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.service.implementation.SeatsBookingSystemServiceImpl;
@@ -16,7 +19,8 @@ import java.util.List;
 @RequestMapping(path = "/seats-booking")
 public class SeatsBookingSystemController {
 
-    private final SeatsBookingSystemServiceImpl seatsBookingSystemService;
+    private static final Logger logger = LoggerFactory.getLogger(SeatsBookingSystemController.class);
+    private final SeatsBookingSystemServiceImpl seatsBookingSystemServiceImpl;
 
     @GetMapping(path = "/test")
     public String test() {
@@ -44,6 +48,14 @@ public class SeatsBookingSystemController {
     }
 
     //Methods related with assigning seats & plane seats schemes to flights:
+
+    @PostMapping(path = "/add-new-seats-model")
+    public void addNewSeatsSchemeModel(@Valid @RequestBody CreateSeatsSchemeModelCommand createSeatsSchemeModelCommand) {
+        String planeModel = createSeatsSchemeModelCommand.planeModel();
+        logger.info("Adding new seats scheme to database for {} plane model.", planeModel);
+        seatsBookingSystemServiceImpl.addNewSeatsSchemeModel(createSeatsSchemeModelCommand);
+        logger.info("Seats scheme for {} added succesfully!!", planeModel);
+    }
 
     @PostMapping(path = "/add-seats-scheme-to-flight")
     public void addSeatsSchemeToFlight() {
