b53931d90591c47ee544d4240ab49b03bca5b821
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-10-25 04:17:17 +0200

    Service Const Strings migration to extracted class
    All Const Strings from SeatsBookingSystemServiceImpl @Service class have
    been moved to newly created ConstsImpl class in implementation package
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

---
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/flight/logic/service/implementation/ConstsImpl.java
@@ -0,0 +1,14 @@
+package pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.service.implementation;
+
+public class ConstsImpl {
+    public static final String SEATS_SCHEME_MODEL_NOT_FOUND_EXCEPTION_PLANE_NAME =
+            "Seats scheme model with specified plane model name: %s has not been found!!!";
+    public static final String SEATS_SCHEME_MODEL_NOT_FOUND_EXCEPTION_ID =
+            "Seats scheme model with specified ID: %s has not been found!!!";
+    public static final String SEATS_SCHEME_ALREADY_EXISTS_EXCEPTION =
+            "Seats scheme model for %s plane model name already exists !!!";
+    public static final String SEATS_SCHEME_MODEL_REMOVAL_ID =
+            "Seats scheme model with ID: %s has been removed from database !!!";
+    public static final String SEATS_SCHEME_MODEL_REMOVAL_PLANE_NAME =
+            "Seats scheme model with %s plane model name has been removed from database !!!";
+}
--- seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/flight/logic/service/implementation/SeatsBookingSystemServiceImpl.java
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/flight/logic/service/implementation/SeatsBookingSystemServiceImpl.java
@@ -19,21 +19,13 @@ import pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.service.Seat
 import java.util.LinkedList;
 import java.util.List;
 
+import static pl.flywithbookedseats.seatsbookingsystemservice.flight.logic.service.implementation.ConstsImpl.*;
+
 @Service
 @RequiredArgsConstructor
 public class SeatsBookingSystemServiceImpl implements SeatsBookingSystemService {
 
     private static final Logger logger = LoggerFactory.getLogger(SeatsBookingSystemService.class);
-    private static final String SEATS_SCHEME_MODEL_NOT_FOUND_EXCEPTION_PLANE_NAME =
-            "Seats scheme model with specified plane model name: %s has not been found!!!";
-    private static final String SEATS_SCHEME_MODEL_NOT_FOUND_EXCEPTION_ID =
-            "Seats scheme model with specified ID: %s has not been found!!!";
-    private static final String SEATS_SCHEME_ALREADY_EXISTS_EXCEPTION =
-            "Seats scheme model for %s plane model name already exists !!!";
-    private static final String SEATS_SCHEME_MODEL_REMOVAL_ID =
-            "Seats scheme model with ID: %s has been removed from database !!!";
-    private static final String SEATS_SCHEME_MODEL_REMOVAL_PLANE_NAME =
-            "Seats scheme model with %s plane model name has been removed from database !!!";
 
     private final SeatsSchemeModelRepository seatsSchemeModelRepository;
     private final CreateSeatsSchemeModelMapper createSeatsSchemeModelMapper;
