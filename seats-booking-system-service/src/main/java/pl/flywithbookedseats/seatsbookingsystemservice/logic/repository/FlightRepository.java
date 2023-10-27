7252f0a2c5b975ea3b1593be348026fd65437936
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-10-27 14:00:34 +0200

    findByFlightName, findByFlightServiceId flightrepo
    findByFlightName & findByFlightServiceId method signatures have been
    implemented in FlightRepository
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/logic/repository/FlightRepository.java
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/logic/repository/FlightRepository.java
@@ -4,9 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.domain.Flight;
 
+import java.util.Optional;
+
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
 
     boolean existsByFlightName(String flightName);
 
+    Optional<Flight> findByFlightName(String flightName);
+
+    Optional<Flight> findByFlightServiceId(Long flightServiceId);
 }
