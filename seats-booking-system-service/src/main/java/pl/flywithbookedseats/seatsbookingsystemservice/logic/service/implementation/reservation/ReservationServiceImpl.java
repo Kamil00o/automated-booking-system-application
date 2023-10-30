a2d0cd0f57367950ec98e22ee7fd657c0ad92fbc
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-10-30 02:59:06 +0100

    Basic Structure of ReservationServiceImpl added
    ReservationServiceImpl class with pattern of basic methods has been
    added to the project
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

---
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/logic/service/implementation/reservation/ReservationServiceImpl.java
@@ -0,0 +1,55 @@
+package pl.flywithbookedseats.seatsbookingsystemservice.logic.service.implementation.reservation;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.command.reservation.CreateReservationCommand;
+import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.command.reservation.UpdateReservationCommand;
+import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.dto.ReservationDto;
+import pl.flywithbookedseats.seatsbookingsystemservice.logic.service.ReservationService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ReservationServiceImpl implements ReservationService {
+
+    @Transactional
+    @Override
+    public ReservationDto createNewReservation(CreateReservationCommand createReservationCommand) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public ReservationDto updateReservationById(UpdateReservationCommand updateReservationCommand) {
+        return null;
+    }
+
+    @Override
+    public List<ReservationDto> retrieveAllReservations() {
+        return null;
+    }
+
+    @Override
+    public ReservationDto retrieveReservationById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<ReservationDto> retrieveReservationByEmail(String email) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public void deleteAllReservations() {
+
+    }
+
+    @Transactional
+    @Override
+    public void deleteReservationById(Long id) {
+
+    }
+}
