262e5b503ebcaa7e7e0941fa862bd7c0cffa3174
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-11-04 20:40:10 +0100

    ReservationServiceImpl Business logic extracted
    ReservationServiceImpl business logic included in private method within
    the boundaries of ReservationServiceImpl class has been moved to the new
    ReservationBusinessLogic class to make private methods public and enable
    using them by another @Service classes in the entire microservice
    
    Code in ReservationServiceImpl has been optimized too.
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/logic/service/implementation/reservation/ReservationServiceImpl.java
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/logic/service/implementation/reservation/ReservationServiceImpl.java
@@ -4,24 +4,20 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.exceptions.FlightAlreadyExistsException;
-import pl.flywithbookedseats.seatsbookingsystemservice.logic.exceptions.ReservationDatabaseIsEmptyException;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.exceptions.ReservationNotFoundException;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.mapper.reservation.CreateReservationMapper;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.mapper.reservation.ReservationDtoMapper;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.command.BookingEnterDataCommand;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.command.reservation.CreateReservationCommand;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.command.reservation.UpdateReservationCommand;
-import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.domain.Passenger;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.domain.Reservation;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.dto.ReservationDto;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.repository.ReservationRepository;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.service.ReservationService;
 import pl.flywithbookedseats.seatsbookingsystemservice.logic.service.implementation.passenger.PassengerServiceImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static pl.flywithbookedseats.seatsbookingsystemservice.logic.service.implementation.reservation.ReservationConstsImpl.*;
@@ -36,6 +32,7 @@ public class ReservationServiceImpl implements ReservationService {
     private final PassengerServiceImpl passengerService;
     private final CreateReservationMapper createReservationMapper;
     private final ReservationDtoMapper reservationDtoMapper;
+    private final ReservationBusinessLogic reservationBL;
 
     @Transactional
     public ReservationDto bookSeatsInThePlane(BookingEnterDataCommand bookingEnterDataCommand) {
@@ -46,22 +43,21 @@ public class ReservationServiceImpl implements ReservationService {
     @Transactional
     @Override
     public ReservationDto addNewReservationToDb(CreateReservationCommand createReservationCommand) {
-        if (!exists(createReservationCommand)) {
-            return reservationDtoMapper.apply(generateNewReservation(createReservationCommand));
+        if (!reservationBL.exists(createReservationCommand)) {
+            return reservationDtoMapper.apply(reservationBL.generateNewReservation(createReservationCommand));
+        } else {
+            logger.warn(RESERVATION_NOT_CREATED);
+            throw new FlightAlreadyExistsException(RESERVATION_ALREADY_EXISTS_SEAT_NUMBER
+                    .formatted(createReservationCommand.seatNumber()));
         }
-        logger.warn(RESERVATION_NOT_CREATED);
-        throw new FlightAlreadyExistsException(RESERVATION_ALREADY_EXISTS_SEAT_NUMBER
-                .formatted(createReservationCommand.seatNumber()));
     }
 
     @Transactional
     @Override
     public ReservationDto updateReservationById(UpdateReservationCommand updateReservationCommand, Long id) {
-        String passengerEmail = updateReservationCommand.passengerEmail();
-
-        if (!exists(updateReservationCommand)) {
-            return reservationDtoMapper.apply(updateSpecifiedReservation(updateReservationCommand,
-                    retrieveReservationEntityFromDb(id), passengerEmail));
+        if (!reservationBL.exists(updateReservationCommand)) {
+            return reservationDtoMapper.apply(reservationBL.updateSpecifiedReservation(updateReservationCommand,
+                    reservationBL.retrieveReservationEntityFromDb(id), updateReservationCommand.passengerEmail()));
         } else {
             logger.warn(RESERVATION_NOT_UPDATED.formatted(id));
             throw new FlightAlreadyExistsException(RESERVATION_ALREADY_EXISTS_SEAT_NUMBER
@@ -71,20 +67,19 @@ public class ReservationServiceImpl implements ReservationService {
 
     @Override
     public List<ReservationDto> retrieveAllReservations() {
-        List<Reservation> savedReservationList = reservationRepository.findAll();
-        return convertIntoListReservationDto(savedReservationList);
+        return reservationBL.convertIntoListReservationDto(reservationRepository.findAll());
     }
 
     @Override
     public ReservationDto retrieveReservationById(Long id) {
-        return reservationDtoMapper.apply(retrieveReservationEntityFromDb(id));
+        return reservationDtoMapper.apply(reservationBL.retrieveReservationEntityFromDb(id));
     }
 
     @Override
     public List<ReservationDto> retrieveReservationByEmail(String email) {
         List<Reservation> savedReservationList = reservationRepository.findAllByPassengerEmail(email);
         if (!savedReservationList.isEmpty()) {
-            return convertIntoListReservationDto(savedReservationList);
+            return reservationBL.convertIntoListReservationDto(savedReservationList);
         } else {
             logger.warn(RESERVATIONS_NOT_RETRIEVED);
             throw new ReservationNotFoundException(RESERVATION_NOT_FOUND_EMAIL.formatted(email));
@@ -100,64 +95,6 @@ public class ReservationServiceImpl implements ReservationService {
     @Transactional
     @Override
     public void deleteReservationById(Long id) {
-        reservationRepository.delete(retrieveReservationEntityFromDb(id));
-    }
-
-    private void setPassengerDataToReservation(String passengerEmail, Reservation reservation) {
-        Passenger savedPassenger = passengerService.retrievePassengerEntityFromDb(passengerEmail);
-        reservation.setPassenger(savedPassenger);
-    }
-
-    private List<ReservationDto> convertIntoListReservationDto(List<Reservation> localSavedReservationList) {
-        if (!localSavedReservationList.isEmpty()) {
-            List<ReservationDto> savedReservationDtoList = new ArrayList<>();
-            localSavedReservationList.forEach(reservation -> savedReservationDtoList
-                    .add(reservationDtoMapper.apply(reservation)));
-            return savedReservationDtoList;
-        } else {
-            logger.warn(RESERVATIONS_NOT_RETRIEVED);
-            throw new ReservationDatabaseIsEmptyException(RESERVATION_DATABASE_IS_EMPTY_EXCEPTION);
-        }
-    }
-
-    private Reservation generateNewReservation(CreateReservationCommand createReservationCommand) {
-        Reservation newReservation = createReservationMapper.apply(createReservationCommand);
-        String passengerEmail = newReservation.getPassengerEmail();
-        if (passengerService.exists(passengerEmail)) {
-            setPassengerDataToReservation(passengerEmail, newReservation);
-            reservationRepository.save(newReservation);
-            logger.info(RESERVATION_ADDED_PASSENGER);
-        } else {
-            reservationRepository.save(newReservation);
-            logger.info(RESERVATION_ADDED_NO_PASSENGER);
-        }
-
-        return newReservation;
-    }
-
-    private Reservation updateSpecifiedReservation(UpdateReservationCommand updateReservationCommand,
-                                                   Reservation savedReservation, String passengerEmail) {
-        savedReservation.setSeatNumber(updateReservationCommand.seatNumber());
-        savedReservation.setSeatTypeClass(updateReservationCommand.seatTypeClass());
-        savedReservation.setPassengerEmail(passengerEmail);
-        if (passengerService.exists(passengerEmail)) {
-            setPassengerDataToReservation(passengerEmail, savedReservation);
-        }
-        reservationRepository.saveAndFlush(savedReservation);
-
-        return savedReservation;
-    }
-
-    private Reservation retrieveReservationEntityFromDb(Long id) {
-        return reservationRepository.findById(id)
-                .orElseThrow(() -> new ReservationNotFoundException(RESERVATION_NOT_FOUND_ID.formatted(id)));
-    }
-
-    private boolean exists(UpdateReservationCommand updateReservationCommand) {
-        return reservationRepository.existsBySeatNumber(updateReservationCommand.seatNumber());
-    }
-
-    private boolean exists(CreateReservationCommand createReservationCommand) {
-        return reservationRepository.existsBySeatNumber(createReservationCommand.seatNumber());
+        reservationRepository.delete(reservationBL.retrieveReservationEntityFromDb(id));
     }
 }
