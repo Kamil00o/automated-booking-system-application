bf27f18790c78259fe5d1de3bdee2ab880dec090
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-11-04 16:19:31 +0100

    Redesign
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

---
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/logic/service/implementation/reservation/ReservationBusinessLogic.java
@@ -0,0 +1,9 @@
+package pl.flywithbookedseats.seatsbookingsystemservice.logic.service.implementation.reservation;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@Component
+public class ReservationBusinessLogic {
+}
