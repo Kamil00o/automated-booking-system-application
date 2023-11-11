7971c40e36e22bd34ab1ff3b7bd03a2b5c82b0ef
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-11-11 04:49:37 +0100

    DtoPassengerMapper + PassengerConverter added
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

---
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/logic/mapper/passenger/DtoPassengerMapper.java
@@ -0,0 +1,29 @@
+package pl.flywithbookedseats.seatsbookingsystemservice.logic.mapper.passenger;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.domain.Passenger;
+import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.dto.PassengerDto;
+
+import java.util.function.Function;
+
+@AllArgsConstructor
+@Component
+public class DtoPassengerMapper implements Function<PassengerDto, Passenger> {
+
+    private final PassengerConverter passengerConverter;
+
+    @Override
+    public Passenger apply(PassengerDto passengerDto) {
+        return Passenger.builder()
+                .passengerServiceId(passengerDto.passengerServiceId())
+                .email(passengerDto.email())
+                .name(passengerDto.name())
+                .surname(passengerDto.surname())
+                .birthDate(passengerDto.birthDate())
+                .disability(passengerDto.disability())
+                .reservationsList(passengerConverter.convertReservationIdListToEntityVersion(passengerDto.
+                        reservationsIdList()))
+                .build();
+    }
+}
---
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/seatsbookingsystemservice/logic/mapper/passenger/PassengerConverter.java
@@ -0,0 +1,31 @@
+package pl.flywithbookedseats.seatsbookingsystemservice.logic.mapper.passenger;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import pl.flywithbookedseats.seatsbookingsystemservice.logic.model.domain.Reservation;
+import pl.flywithbookedseats.seatsbookingsystemservice.logic.repository.ReservationRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Component
+public class PassengerConverter {
+
+    private final ReservationRepository reservationRepository;
+
+    public List<Reservation> convertReservationIdListToEntityVersion(List<Long> reservationIdList) {
+        List<Reservation> convertedReservationList = new ArrayList<>();
+        reservationIdList.forEach(id -> {
+            Reservation savedReservation = retrieveReservationEntityFromDb(id);
+            if (savedReservation != null) {
+                convertedReservationList.add(savedReservation);
+            }
+        });
+        return convertedReservationList;
+    }
+
+    private Reservation retrieveReservationEntityFromDb(Long id) {
+        return reservationRepository.findById(id).orElse(null);
+    }
+}
