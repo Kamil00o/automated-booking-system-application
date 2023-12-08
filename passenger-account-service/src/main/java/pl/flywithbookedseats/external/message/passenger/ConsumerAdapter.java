99ba0cf6cb70181dc8600767eb35a9fe7b5e6617
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-12-08 22:24:12 +0100

    ConsumerAdapter added
    ConsumerAdapter with consumeMessage has been added.
    PassengeerDtoEventConsumer is using ConsumerAdapter to trigger
    appropriate service in the PassengerService class.
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

---
+++ passenger-account-service/src/main/java/pl/flywithbookedseats/external/message/passenger/ConsumerAdapter.java
@@ -0,0 +1,23 @@
+package pl.flywithbookedseats.external.message.passenger;
+
+import lombok.RequiredArgsConstructor;
+import pl.flywithbookedseats.api.passenger.PassengerDtoMapper;
+import pl.flywithbookedseats.domain.passenger.Passenger;
+import pl.flywithbookedseats.domain.passenger.PassengerService;
+import pl.flywithbookedseats.domain.passenger.RequestType;
+import pl.flywithbookedseats.kafka.PassengerDtoEvent;
+
+@RequiredArgsConstructor
+public class ConsumerAdapter {
+
+    private final PassengerDtoMapper mapper;
+    private final PassengerService service;
+
+    public void consumeMessage(PassengerDtoEvent passengerDtoEvent) {
+        Passenger receivedPassenger = mapper.toDomain(passengerDtoEvent.getPassengerDto());
+        RequestType receivedRequestType = passengerDtoEvent.getRequestType();
+        if (receivedRequestType == RequestType.UPDATE) {
+            service.updatePassengerAccountByEmail(receivedPassenger.getEmail(), receivedPassenger);
+        }
+    }
+}
--- passenger-account-service/src/main/java/pl/flywithbookedseats/external/message/passenger/PassengerDtoEventConsumer.java
+++ passenger-account-service/src/main/java/pl/flywithbookedseats/external/message/passenger/PassengerDtoEventConsumer.java
@@ -1,13 +1,17 @@
 package pl.flywithbookedseats.external.message.passenger;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import pl.flywithbookedseats.kafka.PassengerDtoEvent;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class PassengerDtoEventConsumer {
+
+    private final ConsumerAdapter consumerAdapter;
 
     @KafkaListener(
             topics = "${spring.kafka.topic-toPassengerService.name}",
@@ -15,5 +19,6 @@ public class PassengerDtoEventConsumer {
     )
     public void consume(PassengerDtoEvent passengerDtoEvent) {
         log.info("Message received: {}", passengerDtoEvent);
+        consumerAdapter.consumeMessage(passengerDtoEvent);
     }
 }
