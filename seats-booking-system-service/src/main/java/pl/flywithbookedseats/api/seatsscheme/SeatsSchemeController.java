68e6de1a6dbd2c08c18b03084c5c6611104fba5b
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-12-15 03:16:03 +0100

    Returning HttpStatus in Controller added
    Each method in SeatsSchemeController in booking-service returns
    HttpStatus type.
    
    Entire SeatsScheme domain has been adjusted to HEX architecture!!!!
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- seats-booking-system-service/src/main/java/pl/flywithbookedseats/api/seatsscheme/SeatsSchemeController.java
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/api/seatsscheme/SeatsSchemeController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.flywithbookedseats.appservices.SeatsSchemeApplicationService;
 import pl.flywithbookedseats.domain.seatsscheme.PageSeatsScheme;
@@ -23,62 +24,73 @@ public class SeatsSchemeController {
     private final PageSeatsSchemeDtoMapper pageMapper;
 
     @PostMapping
-    public SeatsSchemeDto addNewSeatsSchemeModel(
+    public ResponseEntity<SeatsSchemeDto> addNewSeatsSchemeModel(
             @Valid @RequestBody CreateSeatsSchemeCommand createSeatsSchemeCommand) {
         String planeModelName = createSeatsSchemeCommand.planeModelName();
         log.info("Adding new seats scheme to database for {} plane model.", planeModelName);
         SeatsScheme savedSeatsScheme = service
                 .addNewSeatsSchemeModel(commandMapper.toDomain(createSeatsSchemeCommand));
         log.info("Seats scheme for {} added successfully!!", planeModelName);
-        return mapper.toDto(savedSeatsScheme);
+
+        return ResponseEntity.ok(mapper.toDto(savedSeatsScheme));
     }
 
     @GetMapping(path = "/planeModelName/{planeModelName}")
-    public SeatsSchemeDto retrieveSeatsSchemeModelByPlaneModel(@PathVariable String planeModelName) {
+    public ResponseEntity<SeatsSchemeDto> retrieveSeatsSchemeModelByPlaneModel(@PathVariable String planeModelName) {
         log.info("Retrieving seat scheme model data for {} plane model.", planeModelName);
         SeatsScheme retrievedSavedSeatsScheme = service.retrieveSeatsSchemeModelByPlaneModel(planeModelName);
-        return mapper.toDto(retrievedSavedSeatsScheme);
+
+        return ResponseEntity.ok(mapper.toDto(retrievedSavedSeatsScheme));
     }
 
     @GetMapping(path = "/id/{id}")
-    public SeatsSchemeDto retrieveSeatsSchemeModelById(@PathVariable Long id) {
+    public ResponseEntity<SeatsSchemeDto> retrieveSeatsSchemeModelById(@PathVariable Long id) {
         log.info("Retrieving seat scheme model data for plane model with id {}.", id);
         SeatsScheme retrievedSavedSeatsScheme = service.retrieveSeatsSchemeModelById(id);
-        return mapper.toDto(retrievedSavedSeatsScheme);
+
+        return ResponseEntity.ok(mapper.toDto(retrievedSavedSeatsScheme));
     }
 
     @GetMapping
-    public PageSeatsSchemeDto retrieveAllSavedSeatsSchemeModelsFromDb(
+    public ResponseEntity<PageSeatsSchemeDto> retrieveAllSavedSeatsSchemeModelsFromDb(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "7") int size) {
         log.info("Retrieving seat scheme model data for all saved plane models.");
         Pageable pageable = PageRequest.of(page, size);
         PageSeatsScheme savedPageSeatsScheme = service.retrieveAllSavedSeatsSchemeModelsFromDb(pageable);
-        return pageMapper.toDto(savedPageSeatsScheme);
+        
+        return ResponseEntity.ok(pageMapper.toDto(savedPageSeatsScheme));
     }
 
     @PutMapping(path = "/{id}")
-    public SeatsSchemeDto updateSeatsSchemeModel(
+    public ResponseEntity<SeatsSchemeDto> updateSeatsSchemeModel(
             @PathVariable Long id,
             @Valid @RequestBody UpdateSeatsSchemeCommand updateSeatsSchemeCommand) {
         log.info("Updating seat scheme model data with ID: {}.", id);
         SeatsScheme updatedSeatsScheme = service
                 .updateSeatsSchemeModel(id, updateMapper.toDomain(updateSeatsSchemeCommand));
-        return mapper.toDto(updatedSeatsScheme);
+
+        return ResponseEntity.ok(mapper.toDto(updatedSeatsScheme));
     }
 
     @DeleteMapping(path = "/id/{id}")
-    public void deleteSeatsSchemeModelById(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteSeatsSchemeModelById(@PathVariable Long id) {
         service.deleteSeatsSchemeModelById(id);
+
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping(path = "/planeModelName/{planeModelName}")
-    public void deleteSeatsSchemeModelByPlaneModelName(@PathVariable String planeModelName) {
+    public ResponseEntity<Void> deleteSeatsSchemeModelByPlaneModelName(@PathVariable String planeModelName) {
         service.deleteSeatsSchemeModelByPlaneModelName(planeModelName);
+
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping
-    public void deleteAllSavedSeatsSchemeModelsFromDb() {
+    public ResponseEntity<Void> deleteAllSavedSeatsSchemeModelsFromDb() {
         service.deleteAllSavedSeatsSchemeModelsFromDb();
+
+        return ResponseEntity.ok().build();
     }
 }
