0726bbac2c8947e78d290fb2fb66d3790f1d4eac
Kamil Laganowski <laganowski.kamil@gmail.com>
2023-12-23 02:52:53 +0100

    Mapping reservationsIdList fixed !!!improv planned
    Mapping field reservationsIdList: Passenger -> KafkaPassengerDto has
    been fixed in KafkaPassengerDtoMapper.
    
    IMROVEMENT IN THE FUTURE REQUIRED:
    mapping reservationsList in passenger: KafkaPassengerDto -> Passenger
    need to be added!!!!!
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- seats-booking-system-service/src/main/java/pl/flywithbookedseats/external/message/passenger/KafkaPassengerDtoMapper.java
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/external/message/passenger/KafkaPassengerDtoMapper.java
@@ -1,13 +1,28 @@
 package pl.flywithbookedseats.external.message.passenger;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import pl.flywithbookedseats.domain.passenger.Passenger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface KafkaPassengerDtoMapper {
 
+
     Passenger toDomain(KafkaPassengerDto dto);
 
+    @Mapping(source = "domain", target = "reservationsIdList", qualifiedByName = "convertedList")
     KafkaPassengerDto toDto(Passenger domain);
+
+    @Named("convertedList")
+    default List<Long> convertList(Passenger domain) {
+        List<Long> convertedReservationIdList = new ArrayList<>();
+        domain.getReservationsList().forEach(reservation -> convertedReservationIdList.add(reservation.getId()));
+
+        return convertedReservationIdList;
+    }
 
 }
