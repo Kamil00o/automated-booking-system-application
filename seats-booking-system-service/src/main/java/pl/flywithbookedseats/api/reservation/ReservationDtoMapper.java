00325ee9706a3e704142205542d293eef197c744
Kamil Laganowski <laganowski.kamil@gmail.com>
2024-01-03 02:56:17 +0100

    ReservationDtoMapper fixed in booking-service
    mapping passengerEmail has been fixed
    
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- seats-booking-system-service/src/main/java/pl/flywithbookedseats/api/reservation/ReservationDtoMapper.java
+++ seats-booking-system-service/src/main/java/pl/flywithbookedseats/api/reservation/ReservationDtoMapper.java
@@ -8,11 +8,6 @@ import pl.flywithbookedseats.domain.reservation.Reservation;
 @Mapper(componentModel = "spring")
 public interface ReservationDtoMapper {
 
-    @Mapping(source = "domain", target = "passengerEmail", qualifiedByName = "getPassengerEmail")
     ReservationDto toDto(Reservation domain);
 
-    @Named("getPassengerEmail")
-    default String retrievePassengerEmail(Reservation domain) {
-        return domain.getPassenger().getEmail();
-    }
 }
