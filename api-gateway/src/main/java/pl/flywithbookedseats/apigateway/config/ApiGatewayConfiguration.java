0932b6bc6dda1f3e6c04e3bfb9236d16e897bf25
Kamil Laganowski <laganowski.kamil@gmail.com>
2024-01-07 17:00:44 +0100

    JWTAuthenticationFilter added as route.filters
    Signed-off-by: Kamil Laganowski <laganowski.kamil@gmail.com>

--- api-gateway/src/main/java/pl/flywithbookedseats/apigateway/config/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/pl/flywithbookedseats/apigateway/config/ApiGatewayConfiguration.java
@@ -1,12 +1,17 @@
 package pl.flywithbookedseats.apigateway.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.flywithbookedseats.apigateway.filter.JWTAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 public class ApiGatewayConfiguration {
+
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder routeLocatorBuilder) {
@@ -14,12 +19,15 @@ public class ApiGatewayConfiguration {
         return routeLocatorBuilder.routes()
                 .route(predicateSpec -> predicateSpec
                         .path("/passengers/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
                         .uri("lb://passenger-account-service"))
                 .route(predicateSpec -> predicateSpec
                         .path("/seats-booking/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
                         .uri("lb://seats-booking-system-service"))
                 .route(predicateSpec -> predicateSpec
                         .path("/seats-scheme/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
                         .uri("lb://seats-booking-system-service"))
                 .route(predicateSpec -> predicateSpec
                         .path("/auth/**")
